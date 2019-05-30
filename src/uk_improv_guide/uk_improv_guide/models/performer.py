c861bb477b45e9a42346414a0417c07732e3a04b
Salim Fadhley <salimfadhley@gmail.com>
2019-05-30 16:35:56 +0100

    Add some stuff

--- src/uk_improv_guide/uk_improv_guide/models/performer.py
+++ src/uk_improv_guide/uk_improv_guide/models/performer.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Performer(models.Model):
-    name = models.CharField(max_length=100, primary_key=True)
-    facebook_link = models.CharField(max_length=100)
-    contact_email_address = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50, default="")
+    family_name = models.CharField(max_length=50, default="")
+    facebook_link = models.CharField(max_length=100, blank=True, default="")
+    contact_email_address = models.CharField(max_length=100, blank=True, default="")
+
+    def __str__(self)->str:
+        return "%s %s" % (self.first_name, self.family_name)
