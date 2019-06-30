b18b80be90d10bb21b820731bb0fb46e8a5059fa
sal <slpg500a>
2019-06-30 11:22:50 +0100

    Now basic support for images works.

--- src/uk_improv_guide/uk_improv_guide/models/school.py
+++ src/uk_improv_guide/uk_improv_guide/models/school.py
@@ -7,8 +7,8 @@ from django.db import models
 @reversion.register
 class School(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    image = models.ImageField(upload_to="school/", blank=True)
     name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="school/", blank=True)
 
     def __str__(self):
         return self.name
