f39ce940320305e8faa32779ef568e9a620c4904
Salim Fadhley <salimfadhley@gmail.com>
2019-07-01 16:31:10 +0100

    Starting work on OpenGraph

---
+++ src/test_uk_improv_guide/lib/test_opengraph.py
@@ -0,0 +1,12 @@
+from src.uk_improv_guide.uk_improv_guide.lib.opengraph import opengraph_headers
+
+
+def test_opengraph0():
+
+    result = opengraph_headers(
+        title="title",
+        image="http://foo"
+        type="xxx",
+        url="http://bar"
+    )
+
---
+++ src/uk_improv_guide/uk_improv_guide/lib/opengraph.py
@@ -0,0 +1,5 @@
+from typing import Sequence, Tuple
+
+
+def opengraph_headers(title:str, image:str, url:str, type:str)->Sequence[Tuple(str,str)]:
+    return []
