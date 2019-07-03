8aa34c86f3cf9aae14dc59ba2f9f451708a19d5b
sal <slpg500a>
2019-07-03 08:09:16 +0100

    Try to make venues work

--- src/uk_improv_guide/uk_improv_guide/views/venue.py
+++ src/uk_improv_guide/uk_improv_guide/views/venue.py
@@ -14,9 +14,7 @@ def venue(request, id: int):
     this_venue: Venue = get_venue_by_id(id)
     title = this_venue.name
 
-    log.info(pprint.pformat(dir(this_venue)))
-
-    events: Sequence[Event] = this_venue.event_set.all()
+    events: Sequence[Event] = Event.objects.all()
 
     return render(
         request,
