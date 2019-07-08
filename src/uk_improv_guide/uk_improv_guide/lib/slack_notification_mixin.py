4baed7cf1ff6c3af26406bf2d4bbc72ee8ec78ff
sal <slpg500a>
2019-07-08 23:19:25 +0100

    Added eventlet

--- src/uk_improv_guide/uk_improv_guide/lib/slack_notification_mixin.py
+++ src/uk_improv_guide/uk_improv_guide/lib/slack_notification_mixin.py
@@ -2,11 +2,16 @@ import logging
 
 import requests
 
+from src.uk_improv_guide.uk_improv_guide.settings import SLACK_WEB_HOOK
+
 log = logging.getLogger(__name__)
 
 
 class SlackNotificationMixin:
     def save(self, *args, **kwargs):
         result = super(SlackNotificationMixin, self).save(*args, **kwargs)
-        log.warning("**** MAGIC IS ABOUT TO HAPPEN!!!!")
+
+
+        requests.post(url=SLACK_WEB_HOOK, data={"An object was updated!"})
+
         return result
