378d4d32f62bb4b1b8ec19c5d12df464b812e3ef
sal <slpg500a>
2019-07-10 00:11:20 +0100

    Fix a broken link on the home page. Change the site title.

--- src/uk_improv_guide/uk_improv_guide/views/events.py
+++ src/uk_improv_guide/uk_improv_guide/views/events.py
@@ -10,7 +10,7 @@ from uk_improv_guide.models.event import Event, get_events_after_datetime
 def events(request):
     now: datetime.datetime = datetime.datetime.now(tz=timezone("Europe/London"))
     events: Sequence[Event] = get_events_after_datetime(now)
-    title: str = "Events"
+    title: str = "World Improv Guide"
     return render(
         request,
         "events_index.html",
