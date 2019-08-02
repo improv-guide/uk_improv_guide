41ade450cd95a321d5890d64b23e714846fbfab8
Salim Fadhley <salimfadhley@gmail.com>
2019-08-02 18:59:30 +0100

    Make the group if it doesn't exist.

--- src/uk_improv_guide/uk_improv_guide/models/signals.py
+++ src/uk_improv_guide/uk_improv_guide/models/signals.py
@@ -8,10 +8,17 @@ log = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance: User, created, **kwargs):
+def create_user_profile(sender, instance: User, created, default_group_name:str="site_users", **kwargs):
     if created:
         instance.is_staff = True
-        group: Group = Group.objects.get(name="site_administrators")
+
+        try:
+            group: Group = Group.objects.get(name=default_group_name)
+        except Group.model.DoesNotExist:
+            log.warning(f"Set permissions for {default_group_name}")
+            group = Group(name=default_group_name)
+            group.save()
+
         log.info(f"Adding group {group} to user: {instance}")
         instance.groups.add(group)
         instance.save()
