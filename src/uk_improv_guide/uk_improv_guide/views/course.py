b5015845e4606d0c5f7df1fd1781b3f72d011de8
Salim Fadhley <salimfadhley@gmail.com>
2019-08-03 12:32:30 +0100

    Starting to make courses relate to other kinds of object.

--- src/uk_improv_guide/uk_improv_guide/views/course.py
+++ src/uk_improv_guide/uk_improv_guide/views/course.py
@@ -1,28 +1,23 @@
 from django.shortcuts import render
 from uk_improv_guide.lib.opengraph import opengraph_website
-from uk_improv_guide.models.event import (
-    Event,
-    get_event_by_id,
-    get_events_after_datetime,
-)
+from uk_improv_guide.models.course import Course, get_course_by_id
 from uk_improv_guide.models.venue import Venue
 
 
 def course(request, id: int):
-    this_event: Event = get_event_by_id(id)
-    title = f"{this_event.name}"
-    venue: Venue = this_event.venue
+    this_course: Course = get_course_by_id(id)
+    title = f"{this_course.school.name} / {this_course.name}"
+    venue: Venue = this_course.venue
 
     return render(
         request,
         "course.html",
         {
             "title": title,
-            "event": this_event,
+            "course": this_course,
             "venue": venue,
-            "teams": this_event.teams.all(),
             "og": opengraph_website(
-                title=title, request=request, image=this_event.image
+                title=title, request=request, image=this_course.image
             ),
         },
     )
--- src/uk_improv_guide/uk_improv_guide/views/courses.py
+++ src/uk_improv_guide/uk_improv_guide/views/courses.py
@@ -4,19 +4,19 @@ from typing import Sequence
 from django.shortcuts import render
 from pytz import timezone
 from uk_improv_guide.lib.opengraph import opengraph_website
-from uk_improv_guide.models.event import Event, get_events_after_datetime
+from uk_improv_guide.models.course import get_courses_after_datetime, Course
 
 
 def courses(request):
     now: datetime.datetime = datetime.datetime.now(tz=timezone("Europe/London"))
-    events: Sequence[Event] = get_events_after_datetime(now)
+    courses: Sequence[Course] = get_courses_after_datetime(now)
     title: str = "Courses"
     return render(
         request,
         "courses_index.html",
         {
             "title": "Improv Courses",
-            "events": events,
+            "courses": courses,
             "og": opengraph_website(title=title, request=request, image=None),
         },
     )
